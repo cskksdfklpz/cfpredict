d0292cd87cefc367a549a6a9d27c4efa5e2f4df5
cskksdfklpz <cskksdfklpz@gmail.com>
2017-08-22 09:32:27 +0800

    fitst commit

---
+++ cfpredict/__init__.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python
+from .text import ann
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='cfpredict',
+      version='0.1',
+      description='fine-grained prediction of commodity futures price',
+      url='http://github.com/cskksdfklpz',
+      author='Quanzhi Bi',
+      author_email='cskksdfklpz@gmail.com',
+      license='MIT',
+      packages=['cfpredict'],
+      zip_safe=False)
