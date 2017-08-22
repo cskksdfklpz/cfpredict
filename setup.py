011e938d655c8cfc0ae89b928d61e0234c7d832f
cskksdfklpz <cskksdfklpz@gmail.com>
2017-08-22 09:46:02 +0800

    revised setup.py and create MAMIFEST.in

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 from setuptools import setup
 
 setup(name='cfpredict',
       version='0.1',
       description='fine-grained prediction of commodity futures price',
+      long_description=readme(),
       url='http://github.com/cskksdfklpz',
       author='Quanzhi Bi',
       author_email='cskksdfklpz@gmail.com',
